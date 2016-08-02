3c25d9060532980cb463f76804882b1b619271ea
Stefan Froemken <froemken@gmail.com>
2016-08-02 15:25:37 +0200

    [TASK] Remove duplicate jQuery onReady calls

--- Resources/Public/JavaScript/StoragePathModule.js
+++ Resources/Public/JavaScript/StoragePathModule.js
@@ -3,39 +3,37 @@
  * Object that replace pages_ID with ID
  */
 define('TYPO3/CMS/Weather2/StoragePathModule', ['jquery'], function ($) {
-	$(function () {
+	$(document).ready(function () {
+		$('#recordStoragePage').change(function () {
+			$(this).val($(this).val().replace(/[^0-9]/g, ''));
+		});
+
+		var errorNotificationFields = ['mailConfig_row', 'emailSenderName_row', 'emailSender_row', 'emailReceiver_row'];
+
+		$('#errorNotification').click(function () {
+			toggleErrorNotificationFields();
+		});
+
+		toggleErrorNotificationFields();
+
+		function toggleErrorNotificationFields() {
+			if ($('#errorNotification').is(':checked')) {
+				setDisplayAttributeOfElements('', errorNotificationFields);
+			} else {
+				setDisplayAttributeOfElements('none', errorNotificationFields);
+			}
+		}
 
 		/**
 		 * Sets the display property for each element in array elements
+		 *
 		 * @param display display property value from css (e.g. block or none)
 		 * @param elements array of all elements ['first_element', 'second_element']
 		 */
 		function setDisplayAttributeOfElements(display, elements) {
-			TYPO3.jQuery(elements).each(function (index, value) {
-				TYPO3.jQuery('#' + value).css('display', display);
+			$(elements).each(function (index, value) {
+				$('#' + value).css('display', display);
 			});
 		}
-
-		TYPO3.jQuery(document).ready(function () {
-			TYPO3.jQuery('#recordStoragePage').change(function () {
-				TYPO3.jQuery(this).val(TYPO3.jQuery(this).val().replace(/[^0-9]/g, ''));
-			});
-
-			var errorNotificationFields = ['mailConfig_row', 'emailSenderName_row', 'emailSender_row', 'emailReceiver_row'];
-
-			function toggleErrorNotificationFields() {
-				if (TYPO3.jQuery('#errorNotification').is(':checked')) {
-					setDisplayAttributeOfElements('', errorNotificationFields);
-				} else {
-					setDisplayAttributeOfElements('none', errorNotificationFields);
-				}
-			}
-
-			TYPO3.jQuery('#errorNotification').click(function () {
-				toggleErrorNotificationFields();
-			});
-
-			toggleErrorNotificationFields();
-		});
 	});
 });
