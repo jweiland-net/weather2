94e3ee9f3a44d05f4d0b3b8e6a4f038c01fa2a0c
Stefan Froemken <froemken@gmail.com>
2022-07-22 11:56:00 +0200

    Remove TYPO3 columns from ext_tables.sql

--- ext_tables.sql
+++ ext_tables.sql
@@ -2,10 +2,6 @@
 # Table structure for table 'tx_weather2_domain_model_currentweather'
 #
 CREATE TABLE tx_weather2_domain_model_currentweather (
-
-	uid int(11) NOT NULL auto_increment,
-	pid int(11) DEFAULT '0' NOT NULL,
-
 	name varchar(255) DEFAULT '' NOT NULL,
 	measure_timestamp int(11) DEFAULT '0' NOT NULL,
 	temperature_c int(11) DEFAULT '0' NOT NULL,
@@ -22,28 +18,12 @@ CREATE TABLE tx_weather2_domain_model_currentweather (
 	serialized_array varchar(255) DEFAULT '' NOT NULL,
 	icon varchar(30) DEFAULT '' NOT NULL,
 	condition_code int(11) unsigned DEFAULT '0' NOT NULL,
-
-	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
-	crdate int(11) unsigned DEFAULT '0' NOT NULL,
-	cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
-	deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
-	hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
-	starttime int(11) unsigned DEFAULT '0' NOT NULL,
-	endtime int(11) unsigned DEFAULT '0' NOT NULL,
-
-	PRIMARY KEY (uid),
-	KEY parent (pid)
-
 );
 
 #
 # Table structure for table 'tx_weather2_domain_model_weatheralert'
 #
 CREATE TABLE tx_weather2_domain_model_weatheralert (
-
-	uid int(11) NOT NULL auto_increment,
-	pid int(11) DEFAULT '0' NOT NULL,
-
 	dwd_warn_cell int(11) NOT NULL,
 	level int(11) DEFAULT '0' NOT NULL,
 	type int(11) DEFAULT '0' NOT NULL,
@@ -55,40 +35,14 @@ CREATE TABLE tx_weather2_domain_model_weatheralert (
 	end_date int(11) unsigned DEFAULT '0' NOT NULL,
 	comparison_hash varchar(32) DEFAULT '' NOT NULL,
 	preliminary_information tinyint(4) unsigned DEFAULT '0' NOT NULL,
-
-	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
-	crdate int(11) unsigned DEFAULT '0' NOT NULL,
-	cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
-	deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
-	hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
-	starttime int(11) unsigned DEFAULT '0' NOT NULL,
-	endtime int(11) unsigned DEFAULT '0' NOT NULL,
-
-	PRIMARY KEY (uid),
-	KEY parent (pid)
-
 );
 
 #
 # Table structure for table 'tx_weather2_domain_model_dwdwarncell'
 #
 CREATE TABLE tx_weather2_domain_model_dwdwarncell (
-
-	uid int(11) NOT NULL auto_increment,
-	pid int(11) DEFAULT '0' NOT NULL,
-
 	warn_cell_id varchar(30) DEFAULT '' NOT NULL,
 	name varchar(60) DEFAULT '' NOT NULL,
 	short_name varchar(30) DEFAULT '' NOT NULL,
 	sign varchar(10) DEFAULT '' NOT NULL,
-
-	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
-	crdate int(11) unsigned DEFAULT '0' NOT NULL,
-	cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
-	deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
-	hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
-
-	PRIMARY KEY (uid),
-	KEY parent (pid)
-
 );
