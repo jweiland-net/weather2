0068791d35c0d9f39a10d3b736944dbcc8afa3e1
Stefan Froemken <froemken@gmail.com>
2022-07-29 14:48:54 +0200

    Catch exception. Add DocHeader

--- Classes/Domain/Repository/DwdWarnCellRepository.php
+++ Classes/Domain/Repository/DwdWarnCellRepository.php
@@ -12,28 +12,33 @@ declare(strict_types=1);
 namespace JWeiland\Weather2\Domain\Repository;
 
 use JWeiland\Weather2\Domain\Model\DwdWarnCell;
+use TYPO3\CMS\Extbase\Persistence\Exception\InvalidQueryException;
 use TYPO3\CMS\Extbase\Persistence\Repository;
 
 /**
- * DWD warn cell repository
+ * Repository to find warn cells by name
  */
 class DwdWarnCellRepository extends Repository
 {
     /**
-     * Finds objects by properties name and district
-     *
-     * @param string $name
      * @return DwdWarnCell[]
      */
     public function findByName(string $name): array
     {
         $query = $this->createQuery();
-        $query->matching(
-            $query->logicalOr(
-                $query->like('name', '%' . trim($name) . '%'),
-                $query->equals('warn_cell_id', $name)
-            )
-        );
-        return $query->execute()->toArray();
+
+        try {
+            return $query
+                ->matching(
+                    $query->logicalOr(
+                        $query->like('name', '%' . trim($name) . '%'),
+                        $query->equals('warn_cell_id', $name)
+                    )
+                )
+                ->execute()
+                ->toArray();
+        } catch (InvalidQueryException $invalidQueryException) {
+            return [];
+        }
     }
 }
