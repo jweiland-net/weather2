ba55f9ec171d597ec6c9d580291904d93dc5db97
Stefan Froemken <froemken@gmail.com>
2022-07-29 15:14:29 +0200

    Remove annotations from WeatherConverterService

--- Classes/Service/WeatherConverterService.php
+++ Classes/Service/WeatherConverterService.php
@@ -16,46 +16,26 @@ namespace JWeiland\Weather2\Service;
  */
 class WeatherConverterService
 {
-    /**
-     * @param float $value
-     * @return float
-     */
     public function convertFahrenheitToCelsius(float $value): float
     {
         return $value * 5 / 9 - 32;
     }
 
-    /**
-     * @param float $value
-     * @return float
-     */
     public function convertKelvinToCelsius(float $value): float
     {
         return $value - 273.15;
     }
 
-    /**
-     * @param float $value
-     * @return float
-     */
     public function convertCelsiusToFahrenheit(float $value): float
     {
         return $value * 9 / 5 + 32;
     }
 
-    /**
-     * @param float $value
-     * @return float
-     */
     public function convertCelsiusToKelvin(float $value): float
     {
         return $value + 273.15;
     }
 
-    /**
-     * @param float $value
-     * @return string
-     */
     public function convertMetersToMiles(float $value): string
     {
         return number_format($value * 2.236936, 2, '.', '');
