25e3c2ecd5924e2ddce8bca6b09cb87ebff66850
Stefan Froemken <froemken@gmail.com>
2022-09-27 10:03:35 +0200

    Store rain/snow as float in db

--- ext_tables.sql
+++ ext_tables.sql
@@ -2,48 +2,28 @@
 # Table structure for table 'tx_weather2_domain_model_currentweather'
 #
 CREATE TABLE tx_weather2_domain_model_currentweather (
-
-	uid int(11) NOT NULL auto_increment,
-	pid int(11) DEFAULT '0' NOT NULL,
-
 	name varchar(255) DEFAULT '' NOT NULL,
 	measure_timestamp int(11) DEFAULT '0' NOT NULL,
-	temperature_c int(11) DEFAULT '0' NOT NULL,
+	temperature_c double(4,2) DEFAULT '0.0' NOT NULL,
 	pressure_hpa int(11) DEFAULT '0' NOT NULL,
 	humidity_percentage int(11) unsigned DEFAULT '0' NOT NULL,
-	min_temp_c int(11) DEFAULT '0' NOT NULL,
-	max_temp_c int(11) DEFAULT '0' NOT NULL,
-	wind_speed_m_p_s int(11) DEFAULT '0' NOT NULL,
+	min_temp_c double(4,2) DEFAULT '0.0' NOT NULL,
+	max_temp_c double(4,2) DEFAULT '0.0' NOT NULL,
+	wind_speed_m_p_s double(4,2) DEFAULT '0.0' NOT NULL,
 	wind_direction_deg int(11) DEFAULT '0' NOT NULL,
 	pop_percentage int(11) unsigned DEFAULT '0' NOT NULL,
-	rain_volume int(11) DEFAULT '0' NOT NULL,
-	snow_volume int(11) DEFAULT '0' NOT NULL,
+	rain_volume double (4,2) DEFAULT '0.0' NOT NULL,
+	snow_volume double(4,2) DEFAULT '0.0' NOT NULL,
 	clouds_percentage int(11) unsigned DEFAULT '0' NOT NULL,
 	serialized_array varchar(255) DEFAULT '' NOT NULL,
 	icon varchar(30) DEFAULT '' NOT NULL,
-	condition_code int(11) unsigned DEFAULT '0' NOT NULL,
-
-	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
-	crdate int(11) unsigned DEFAULT '0' NOT NULL,
-	cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
-	deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
-	hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
-	starttime int(11) unsigned DEFAULT '0' NOT NULL,
-	endtime int(11) unsigned DEFAULT '0' NOT NULL,
-
-	PRIMARY KEY (uid),
-	KEY parent (pid)
-
+	condition_code int(11) unsigned DEFAULT '0' NOT NULL
 );
 
 #
 # Table structure for table 'tx_weather2_domain_model_weatheralert'
 #
 CREATE TABLE tx_weather2_domain_model_weatheralert (
-
-	uid int(11) NOT NULL auto_increment,
-	pid int(11) DEFAULT '0' NOT NULL,
-
 	dwd_warn_cell int(11) NOT NULL,
 	level int(11) DEFAULT '0' NOT NULL,
 	type int(11) DEFAULT '0' NOT NULL,
@@ -54,41 +34,15 @@ CREATE TABLE tx_weather2_domain_model_weatheralert (
 	start_date int(11) unsigned DEFAULT '0' NOT NULL,
 	end_date int(11) unsigned DEFAULT '0' NOT NULL,
 	comparison_hash varchar(32) DEFAULT '' NOT NULL,
-	preliminary_information tinyint(4) unsigned DEFAULT '0' NOT NULL,
-
-	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
-	crdate int(11) unsigned DEFAULT '0' NOT NULL,
-	cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
-	deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
-	hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
-	starttime int(11) unsigned DEFAULT '0' NOT NULL,
-	endtime int(11) unsigned DEFAULT '0' NOT NULL,
-
-	PRIMARY KEY (uid),
-	KEY parent (pid)
-
+	preliminary_information tinyint(4) unsigned DEFAULT '0' NOT NULL
 );
 
 #
 # Table structure for table 'tx_weather2_domain_model_dwdwarncell'
 #
 CREATE TABLE tx_weather2_domain_model_dwdwarncell (
-
-	uid int(11) NOT NULL auto_increment,
-	pid int(11) DEFAULT '0' NOT NULL,
-
 	warn_cell_id varchar(30) DEFAULT '' NOT NULL,
 	name varchar(60) DEFAULT '' NOT NULL,
 	short_name varchar(30) DEFAULT '' NOT NULL,
-	sign varchar(10) DEFAULT '' NOT NULL,
-
-	tstamp int(11) unsigned DEFAULT '0' NOT NULL,
-	crdate int(11) unsigned DEFAULT '0' NOT NULL,
-	cruser_id int(11) unsigned DEFAULT '0' NOT NULL,
-	deleted tinyint(4) unsigned DEFAULT '0' NOT NULL,
-	hidden tinyint(4) unsigned DEFAULT '0' NOT NULL,
-
-	PRIMARY KEY (uid),
-	KEY parent (pid)
-
+	sign varchar(10) DEFAULT '' NOT NULL
 );
