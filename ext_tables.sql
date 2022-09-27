d27b13fe5727c505d7d5bc30cf1cf8b6a5868b88
Stefan Froemken <froemken@gmail.com>
2022-09-27 08:40:36 +0200

    Set various columns to float values

--- ext_tables.sql
+++ ext_tables.sql
@@ -4,20 +4,20 @@
 CREATE TABLE tx_weather2_domain_model_currentweather (
 	name varchar(255) DEFAULT '' NOT NULL,
 	measure_timestamp int(11) DEFAULT '0' NOT NULL,
-	temperature_c int(11) DEFAULT '0' NOT NULL,
+	temperature_c double(4,2) DEFAULT '0.0' NOT NULL,
 	pressure_hpa int(11) DEFAULT '0' NOT NULL,
 	humidity_percentage int(11) unsigned DEFAULT '0' NOT NULL,
-	min_temp_c int(11) DEFAULT '0' NOT NULL,
-	max_temp_c int(11) DEFAULT '0' NOT NULL,
-	wind_speed_m_p_s int(11) DEFAULT '0' NOT NULL,
+	min_temp_c double(4,2) DEFAULT '0.0' NOT NULL,
+	max_temp_c double(4,2) DEFAULT '0.0' NOT NULL,
+	wind_speed_m_p_s double(4,2) DEFAULT '0.0' NOT NULL,
 	wind_direction_deg int(11) DEFAULT '0' NOT NULL,
 	pop_percentage int(11) unsigned DEFAULT '0' NOT NULL,
-	rain_volume int(11) DEFAULT '0' NOT NULL,
-	snow_volume int(11) DEFAULT '0' NOT NULL,
+	rain_volume double (4,2) DEFAULT '0.0' NOT NULL,
+	snow_volume double(4,2) DEFAULT '0.0' NOT NULL,
 	clouds_percentage int(11) unsigned DEFAULT '0' NOT NULL,
 	serialized_array varchar(255) DEFAULT '' NOT NULL,
 	icon varchar(30) DEFAULT '' NOT NULL,
-	condition_code int(11) unsigned DEFAULT '0' NOT NULL,
+	condition_code int(11) unsigned DEFAULT '0' NOT NULL
 );
 
 #
@@ -34,7 +34,7 @@ CREATE TABLE tx_weather2_domain_model_weatheralert (
 	start_date int(11) unsigned DEFAULT '0' NOT NULL,
 	end_date int(11) unsigned DEFAULT '0' NOT NULL,
 	comparison_hash varchar(32) DEFAULT '' NOT NULL,
-	preliminary_information tinyint(4) unsigned DEFAULT '0' NOT NULL,
+	preliminary_information tinyint(4) unsigned DEFAULT '0' NOT NULL
 );
 
 #
@@ -44,5 +44,5 @@ CREATE TABLE tx_weather2_domain_model_dwdwarncell (
 	warn_cell_id varchar(30) DEFAULT '' NOT NULL,
 	name varchar(60) DEFAULT '' NOT NULL,
 	short_name varchar(30) DEFAULT '' NOT NULL,
-	sign varchar(10) DEFAULT '' NOT NULL,
+	sign varchar(10) DEFAULT '' NOT NULL
 );
