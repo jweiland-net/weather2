755f409d31f9ed4743ec54a9128ffaf2151c4cd4
Stefan Froemken <froemken@gmail.com>
2023-02-20 08:38:18 +0100

    Use let instead of var in JS files

--- Resources/Public/JavaScript/OpenWeatherMapTaskModule.js
+++ Resources/Public/JavaScript/OpenWeatherMapTaskModule.js
@@ -3,37 +3,37 @@
  * JavaScript to show/hide fields in scheduler
  */
 define('TYPO3/CMS/Weather2/OpenWeatherMapTaskModule', ['jquery'], function ($) {
-	$(document).ready(function () {
+  $(document).ready(function () {
     $('#recordStoragePage').change(function () {
       $(this).val($(this).val().replace(/[^0-9]/g, ''));
     });
-    
-		var errorNotificationFields = ['mailConfig_row', 'emailSenderName_row', 'emailSender_row', 'emailReceiver_row'];
 
-		$('#errorNotification').click(function () {
-			toggleErrorNotificationFields();
-		});
+    let errorNotificationFields = ['mailConfig_row', 'emailSenderName_row', 'emailSender_row', 'emailReceiver_row'];
 
-		toggleErrorNotificationFields();
+    $('#errorNotification').click(function () {
+      toggleErrorNotificationFields();
+    });
 
-		function toggleErrorNotificationFields() {
-			if ($('#errorNotification').is(':checked')) {
-				setDisplayAttributeOfElements('', errorNotificationFields);
-			} else {
-				setDisplayAttributeOfElements('none', errorNotificationFields);
-			}
-		}
-    
+    toggleErrorNotificationFields();
+
+    function toggleErrorNotificationFields () {
+      if ($('#errorNotification').is(':checked')) {
+        setDisplayAttributeOfElements('', errorNotificationFields);
+      } else {
+        setDisplayAttributeOfElements('none', errorNotificationFields);
+      }
+    }
+
     /**
      * Sets the display property for each element in array elements
      *
      * @param display display property value from css (e.g. block or none)
      * @param elements array of all elements ['first_element', 'second_element']
      */
-    function setDisplayAttributeOfElements(display, elements) {
+    function setDisplayAttributeOfElements (display, elements) {
       $(elements).each(function (index, value) {
         $('#' + value).css('display', display);
       });
     }
-	});
+  });
 });
