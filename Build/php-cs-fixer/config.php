b05913a21af346c7780d85b743a1afdf38658245
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2023-11-15 13:26:21 +0100

    CGL fix

--- Build/php-cs-fixer/config.php
+++ Build/php-cs-fixer/config.php
@@ -7,7 +7,7 @@ declare(strict_types=1);
  *
  * For the full copyright and license information, please read the
  * LICENSE file that was distributed with this source code.
-*/
+ */
 
 use PhpCsFixer\Config;
 use PhpCsFixer\Finder;
