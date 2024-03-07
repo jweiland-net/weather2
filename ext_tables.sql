b79c78ce8365802a004d08823b57445841f9e44a
Markus Ketterer <markus.ketterer@gugler.at>
2024-03-07 13:10:56 +0100

    [Feature] retrieve l18n Description from currentWeather by openweatherapi

--- ext_tables.sql
+++ ext_tables.sql
@@ -3,6 +3,7 @@
 #
 CREATE TABLE tx_weather2_domain_model_currentweather (
 	name varchar(255) DEFAULT '' NOT NULL,
+	description varchar(255) DEFAULT '' NOT NULL,
 	measure_timestamp int(11) DEFAULT '0' NOT NULL,
 	temperature_c double(4,2) DEFAULT '0.0' NOT NULL,
 	pressure_hpa double(4,2) DEFAULT '0' NOT NULL,
