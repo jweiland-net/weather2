327f1a7efcdd43b5373726143868721fdeac12ff
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2024-10-31 17:47:18 +0100

    [TASK] Moved icon configurations to Configuration/Icons.php and also moved pageTs configuration to Configuration folder

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -1,8 +1,23 @@
 <?php
 
+use TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider;
+use TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider;
+
 return [
     'ext-weather2' => [
-        'provider' => \TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+        'provider' => SvgIconProvider::class,
         'source' => 'EXT:weather2/Resources/Public/Icons/Extension.svg',
+    ],
+    'ext-weather2-table-currentweather' => [
+        'provider' => BitmapIconProvider::class,
+        'source' => 'EXT:weather2/Resources/Public/Icons/tx_weather2_domain_model_currentweather.gif',
+    ],
+    'ext-weather2-table-dwdwarncell' => [
+        'provider' => BitmapIconProvider::class,
+        'source' => 'EXT:weather2/Resources/Public/Icons/tx_weather2_domain_model_dwdwarncell.gif',
+    ],
+    'ext-weather2-table-weatheralert' => [
+        'provider' => BitmapIconProvider::class,
+        'source' => 'EXT:weather2/Resources/Public/Icons/tx_weather2_domain_model_weatheralert.gif',
     ],
 ];
--- ext_localconf.php
+++ ext_localconf.php
@@ -4,61 +4,50 @@ if (!defined('TYPO3')) {
     die('Access denied.');
 }
 
+use JWeiland\Weather2\Controller\CurrentWeatherController;
+use JWeiland\Weather2\Controller\WeatherAlertController;
+use JWeiland\Weather2\Task\DeutscherWetterdienstTask;
+use JWeiland\Weather2\Task\DeutscherWetterdienstTaskAdditionalFieldProvider;
+use JWeiland\Weather2\Task\DeutscherWetterdienstWarnCellTask;
+use JWeiland\Weather2\Task\OpenWeatherMapTask;
+use JWeiland\Weather2\Task\OpenWeatherMapTaskAdditionalFieldProvider;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
 call_user_func(static function () {
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][JWeiland\Weather2\Task\OpenWeatherMapTask::class] = [
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][OpenWeatherMapTask::class] = [
         'extension' => 'weather2',
         'title' => 'Call openweathermap.org api',
         'description' => 'Calls the api of openweathermap.org and saves response into database',
-        'additionalFields' => JWeiland\Weather2\Task\OpenWeatherMapTaskAdditionalFieldProvider::class,
+        'additionalFields' => OpenWeatherMapTaskAdditionalFieldProvider::class,
     ];
 
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][JWeiland\Weather2\Task\DeutscherWetterdienstTask::class] = [
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][DeutscherWetterdienstTask::class] = [
         'extension' => 'weather2',
         'title' => 'Get weather alerts from Deutscher Wetterdienst',
         'description' => 'Calls the Deutscher Wetterdienst api and saves response in weather2 format into database',
-        'additionalFields' => JWeiland\Weather2\Task\DeutscherWetterdienstTaskAdditionalFieldProvider::class,
+        'additionalFields' => DeutscherWetterdienstTaskAdditionalFieldProvider::class,
     ];
 
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][JWeiland\Weather2\Task\DeutscherWetterdienstWarnCellTask::class] = [
+    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks'][DeutscherWetterdienstWarnCellTask::class] = [
         'extension' => 'weather2',
         'title' => 'Get warn cell records from Deutscher Wetterdienst',
         'description' => 'Calls the Deutscher Wetterdienst api and saves warn cells into database. Required before using DeutscherWetterdienstTask!',
     ];
 
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+    ExtensionUtility::configurePlugin(
         'Weather2',
         'Currentweather',
         [
-            \JWeiland\Weather2\Controller\CurrentWeatherController::class => 'show',
+            CurrentWeatherController::class => 'show',
         ],
     );
 
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
+    ExtensionUtility::configurePlugin(
         'Weather2',
         'Weatheralert',
         [
-            \JWeiland\Weather2\Controller\WeatherAlertController::class => 'show',
+            WeatherAlertController::class => 'show',
         ],
     );
-
-    // Add weather2 plugins to new element wizard
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-        '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:weather2/Configuration/TSconfig/ContentElementWizard.tsconfig">',
-    );
-
-    // Register Bitmap Icon Identifier
-    $bmpIcons = [
-        'ext-weather2-table-currentweather' => 'tx_weather2_domain_model_currentweather.gif',
-        'ext-weather2-table-dwdwarncell' => 'tx_weather2_domain_model_dwdwarncell.gif',
-        'ext-weather2-table-weatheralert' => 'tx_weather2_domain_model_weatheralert.gif',
-    ];
-
-    $iconRegistry = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class);
-    foreach ($bmpIcons as $identifier => $fileName) {
-        $iconRegistry->registerIcon(
-            $identifier,
-            \TYPO3\CMS\Core\Imaging\IconProvider\BitmapIconProvider::class,
-            ['source' => 'EXT:weather2/Resources/Public/Icons/' . $fileName],
-        );
-    }
 });
