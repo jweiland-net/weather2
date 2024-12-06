4edff8ef90d89b148210241483fcb00d336f7524
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2024-12-06 15:01:58 +0100

    [TASK] Documenation updates

--- Configuration/Backend/AjaxRoutes.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'weather2_dwd_warn-cell-search' => [
-        'path' => '/weather2/dwd/warn-cell-search',
-        'target' => \JWeiland\Weather2\Ajax\DeutscherWetterdienstWarnCellSearch::class . '::renderWarnCells',
-    ],
-];
